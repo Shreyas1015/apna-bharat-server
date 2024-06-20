aaf34d2927f4731111d869cf80ab9e0c1db84b12
Shreyas <shreyas1234gurav@gmail.com>
2024-06-21 02:46:49 +0530

    url added

--- src/middlewares/corsMiddleware.js
+++ src/middlewares/corsMiddleware.js
@@ -6,19 +6,19 @@
 
 // module.exports = corsOptions;
 const cors = require("cors");
-const allowedOrigins = [process.env.FRONTEND_URL];
+const allowedOrigins = ["https://apna-bharat-client.vercel.app"];
 
 const corsOptions = cors({
   origin: (origin, callback) => {
-    console.log("Request received from origin:", origin); // Log the origin of each request
+    console.log("Request received from origin:", origin);
     if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true); // Allow the request if the origin is in the allowed list or not present (server-to-server requests)
+      callback(null, true);
     } else {
       console.error("Blocked by CORS policy:", origin);
-      callback(new Error("Not allowed by CORS"), false); // Block the request if the origin is not allowed
+      callback(new Error("Not allowed by CORS"), false);
     }
   },
-  credentials: true, // Allow credentials like cookies, authorization headers, etc.
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+  optionsSuccessStatus: 200,
 });
 module.exports = corsOptions;
